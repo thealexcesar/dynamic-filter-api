8ee5e69a016ea48bb9811c6e3b542d2443c943e2
alex cesar <thealexcesar@gmail.com>
2024-08-22 16:33:47 -0300

    Initial project seetup: Spring Boot

---
+++ src/main/java/com/dynamicfilteringapi/DynamicFilteringApiApplication.java
@@ -0,0 +1,13 @@
+package com.dynamicfilteringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicFilteringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicFilteringApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dynamicfilteringapi/DynamicFilteringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamicfilteringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicFilteringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
